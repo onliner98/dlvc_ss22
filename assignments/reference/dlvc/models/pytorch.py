60346d3a9323e62840557048f6401a963df4e68c
Strohmayer Julian <jstrohmayer@cvl.tuwien.ac.at>
2022-03-16 17:20:31 +0000

    added reference code

---
+++ assignments/reference/dlvc/models/pytorch.py
@@ -0,0 +1,86 @@
+import numpy as np
+import torch.nn as nn
+
+from ..model import Model
+
+class CnnClassifier(Model):
+    '''
+    Wrapper around a PyTorch CNN for classification.
+    The network must expect inputs of shape NCHW with N being a variable batch size,
+    C being the number of (image) channels, H being the (image) height, and W being the (image) width.
+    The network must end with a linear layer with num_classes units (no softmax).
+    The cross-entropy loss (torch.nn.CrossEntropyLoss) and SGD (torch.optim.SGD) are used for training.
+    '''
+
+    def __init__(self, net: nn.Module, input_shape: tuple, num_classes: int, lr: float, wd: float):
+        '''
+        Ctor.
+        net is the cnn to wrap. see above comments for requirements.
+        input_shape is the expected input shape, i.e. (0,C,H,W).
+        num_classes is the number of classes (> 0).
+        lr: learning rate to use for training (SGD with e.g. Nesterov momentum of 0.9).
+        wd: weight decay to use for training.
+        '''
+
+        # TODO implement
+
+        # Inside the train() and predict() functions you will need to know whether the network itself
+        # runs on the CPU or on a GPU, and in the latter case transfer input/output tensors via cuda() and cpu().
+        # To termine this, check the type of (one of the) parameters, which can be obtained via parameters() (there is an is_cuda flag).
+        # You will want to initialize the optimizer and loss function here.
+        # Note that PyTorch's cross-entropy loss includes normalization so no softmax is required
+
+        pass
+
+    def input_shape(self) -> tuple:
+        '''
+        Returns the expected input shape as a tuple.
+        '''
+
+        # TODO implement
+
+        pass
+
+    def output_shape(self) -> tuple:
+        '''
+        Returns the shape of predictions for a single sample as a tuple, which is (num_classes,).
+        '''
+
+        # TODO implement
+
+        pass
+
+    def train(self, data: np.ndarray, labels: np.ndarray) -> float:
+        '''
+        Train the model on batch of data.
+        Data has shape (m,C,H,W) and type np.float32 (m is arbitrary).
+        Labels has shape (m,) and integral values between 0 and num_classes - 1.
+        Returns the training loss.
+        Raises TypeError on invalid argument types.
+        Raises ValueError on invalid argument values.
+        Raises RuntimeError on other errors.
+        '''
+
+        # TODO implement
+        # Make sure to set the network to train() mode
+        # See above comments on CPU/GPU
+
+        pass
+
+    def predict(self, data: np.ndarray) -> np.ndarray:
+        '''
+        Predict softmax class scores from input data.
+        Data has shape (m,C,H,W) and type np.float32 (m is arbitrary).
+        The scores are an array with shape (n, output_shape()).
+        Raises TypeError on invalid argument types.
+        Raises ValueError on invalid argument values.
+        Raises RuntimeError on other errors.
+        '''
+
+        # TODO implement
+
+        # Pass the network's predictions through a nn.Softmax layer to obtain softmax class scores
+        # Make sure to set the network to eval() mode
+        # See above comments on CPU/GPU
+
+        pass
---
+++ assignments/reference/dlvc/ops.py
@@ -0,0 +1,86 @@
+from typing import List, Callable
+
+import numpy as np
+
+# All operations are functions that take and return numpy arrays
+# See https://docs.python.org/3/library/typing.html#typing.Callable for what this line means
+Op = Callable[[np.ndarray], np.ndarray]
+
+def chain(ops: List[Op]) -> Op:
+    '''
+    Chain a list of operations together.
+    '''
+
+    def op(sample: np.ndarray) -> np.ndarray:
+        for op_ in ops:
+            sample = op_(sample)
+        return sample
+
+    return op
+
+def type_cast(dtype: np.dtype) -> Op:
+    '''
+    Cast numpy arrays to the given type.
+    '''
+
+    # TODO implement (see above for guidance).
+
+    pass
+
+def vectorize() -> Op:
+    '''
+    Vectorize numpy arrays via "numpy.ravel()".
+    '''
+
+    # TODO implement (see above for guidance).
+
+    pass
+
+def add(val: float) -> Op:
+    '''
+    Add a scalar value to all array elements.
+    '''
+
+    # TODO implement (see above for guidance).
+
+    pass
+
+def mul(val: float) -> Op:
+    '''
+    Multiply all array elements by the given scalar.
+    '''
+
+    # TODO implement (see above for guidance).
+
+    pass
+
+def hwc2chw() -> Op:
+    '''
+    Flip a 3D array with shape HWC to shape CHW.
+    '''
+
+    # TODO implement (see np.transpose)
+
+    pass
+
+def hflip() -> Op:
+    '''
+    Flip arrays with shape HWC horizontally with a probability of 0.5.
+    '''
+
+    # TODO implement (numpy.flip will be helpful)
+
+    pass
+
+def rcrop(sz: int, pad: int, pad_mode: str) -> Op:
+    '''
+    Extract a square random crop of size sz from arrays with shape HWC.
+    If pad is > 0, the array is first padded by pad pixels along the top, left, bottom, and right.
+    How padding is done is governed by pad_mode, which should work exactly as the 'mode' argument of numpy.pad.
+    Raises ValueError if sz exceeds the array width/height after padding.
+    '''
+
+    # TODO implement
+    # https://numpy.org/doc/stable/reference/generated/numpy.pad.html will be helpful
+
+    pass
